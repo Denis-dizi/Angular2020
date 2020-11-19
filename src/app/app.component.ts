9ed4e2fa8e796b9eebae50d99fd2f1b59ee86aa4
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-20 01:53:42 +0200

    update (2:00:). Added click count funct. Works.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,28 @@ export class AppComponent implements OnInit {
   title = 'My 1-st title in Angular';
   name = 'Di';
   lastName = 'Zi';
-  nmbr: number = 10;
+  nmbr: number = 0;
+  count1: number = 0;
+  count2: number = 0;
 
   ngOnInit() {
     setInterval(() => {
       this.nmbr = Math.random() * 100;
     }, 2000)
   }
+
+// (1:52:)
+triggerClick() {
+  this.count1 ++;
+  console.log(this.count1)
+}
+// (1:56:)
+addOne() {
+  this.count2++;
+}
+remooveOne() {
+  this.count2--;
+}
+
+
 }
