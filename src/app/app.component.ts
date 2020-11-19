8583dbc473994d867f5a0ae864f4406225622483
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-20 01:22:17 +0200

    update (1:50:) first steps. working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'web';
+export class AppComponent implements OnInit {
+  title = 'My 1-st title in Angular';
+  name = 'Di';
+  lastName = 'Zi';
+  nmbr: number = 10;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.nmbr = Math.random() * 100;
+    }, 2000)
+  }
 }
