22114ed86d676a57482a27f156f5d9955c0687e6
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-20 16:36:08 +0200

    update (2:58:) Ad non strict class.Works. W9D1 END

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,13 @@ export class AppComponent implements OnInit {
   // (2:42)
   password = '';
   // (2:19)
-  users: any = [
+  // users: any = [
+  // (2:53:)
+  users: User[] = [
     {
       name: "Name-1",
-      lastName: "Lastname-1"
+      lastName: "Lastname-1",
+      age: 12 //(2:56:)
     }, {
       name: "Name-2",
       lastName: "Lastname-2"
@@ -28,8 +31,10 @@ export class AppComponent implements OnInit {
     }
     // (2:36:)
     , {
-      name: "Coolyo"
+      name: "Coolyo",
+      lastName: ''
     }, {
+      name: '',
       lastName: "G-Paradise"
     }
   ];
@@ -57,5 +62,22 @@ remooveOne() {
 clearAllClicks() {
   this.count2 = 0;
 }
+}
 
+// (2:53:)
+class User {
+  name: string;
+  // lastName: string;
+  lastName ?: string;
+  age ? : number; //(2:56:)
+
+  constructor(parameters: any) {
+    // this.name = parameters.name;
+    // this.lastName = parameters.lastName;
+    // (2:58:)
+    this.name = parameters.name || 'unspecified name';
+    this.lastName = parameters.lastName || 'unspecified LNmae';
+    this.age = parameters.age || 'unspecified age'; //(2:56:)
+
+  }
 }
