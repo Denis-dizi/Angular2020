63b0163df1c906bf9aa5df6f07dcf1750e6bb799
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-24 19:44:14 +0200

    update W9D2. Array from list.php is working.

--- api/products/list.php
+++ api/products/list.php
@@ -1,11 +1,10 @@
-<!-- (0:17:) W9D2  -->
-test 1 v2 <br>
+test 1 v2. W9D2 <br>
 http://localhost/web/api/products/list.php <br><hr>
 <?php
-// (0:17:) simulation without real request
+// (0:17:)W9D2 simulation without real request
 class ProductList {
     public function show() {
-        $response = [];
+        $response = [1,2,3];
         echo json_encode($response);
     }
 }
