0f98afc730e4effafccaae9213923f51ffe27eba
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-25 06:07:26 +0200

    update (0:57:) Get data from DB. Works.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'web';
+export class AppComponent implements OnInit {
+
+  //(0:25/52:) W9D2
+  // list = [];
+  list: any[] = []; //(:52:)
+
+  //(0:25:) W9D2
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  //(0:28:) W9D2
+  ngOnInit() {
+    this.http
+      .get('http://localhost/web/api/products/list.php')
+      .toPromise()
+      .then(
+        (response: any) => {
+          this.list = response;
+          console.log(response)
+          console.log('is found')
+
+        },
+        (reject) => {
+          console.log(reject)
+          console.log('not found')
+          alert(reject)
+        }
+      )
+  }
 }
