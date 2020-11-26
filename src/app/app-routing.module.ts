76c7402544e40c0f4177b44df1e2064d5f915b69
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-26 18:55:21 +0200

    update (:30:) use hash. login. registr examples

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  // (:26:)W10D1
+  path: 'login',
+  component: LoginComponent
+},{
+  path: 'register',
+  component: RegisterComponent
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // (:28:)W10D1
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
