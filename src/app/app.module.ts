2fed27679d2cc19d408c1ce2cbdc809cd7346b1c
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-27 00:53:16 +0200

    update (1:37:) Added user-form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProductsListComponent } from './products/list/list.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
