672a8f2d792edf4d08f72594dbe28751298a59f5
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-27 00:49:28 +0200

    update (1:33:) Added AccessManagment. Doe' nt wrk.

--- api/users/hasAccess.php
+++ api/users/hasAccess.php
@@ -1,4 +1,5 @@
 <?php
-// (1:18:)W10D1
+// (1:18/33:)W10D1
+session_start();
 
-?>
+echo json_encode(["has_access" => $_SESSION["id"]]);
--- src/app/helpers/access-managment.guard.ts
+++ src/app/helpers/access-managment.guard.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,19 +9,30 @@ import { Observable } from 'rxjs';
 export class AccessManagmentGuard implements CanActivate {
   // (1:15:) W10D1
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    // (1:23:)W10D1
+    private router: Router
   ) { }
 
-  canActivate(
+  async canActivate( //(1:20:)W10D1
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ) {
     // (1:15:) W10D1
-    this.http
-    .get('http://localhost/web/api/users/hasAccess.php')
-    .toPromise()
+    // this.http.get('http://localhost/web/api/users/hasAccess.php').toPromise()
+    // (1:20:) W10D1
+    const hasAccess = await this.http.get('http://localhost/web/api/users/hasAccess.php')
+      .toPromise().then((response: any) => {
+        return response.has_access
+      });
 
+    console.log(hasAccess);
 
-    return true;
+    if (hasAccess) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
   }
-
-}
+} 
