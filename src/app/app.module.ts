c78db12ea12e2c1b0250d9402fd53b10f5c4be8d
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-26 18:16:50 +0200

    ProductsComponent (0:21:) ng g component products/list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ListComponent } from './products/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
