2597c9e94947c78984f6c2bbd35a58786d29a123
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-27 06:44:59 +0200

    update (2:24:) Added LogOut. works partially. END W10D1

---
+++ api/users/logOut.php
@@ -0,0 +1,4 @@
+<?php
+// (2:23:)W10D1
+session_start();
+session_destroy();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // (2:20:)W10D1
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  logOut() {
+    console.log("trigger log out");
+    this.http.get('http://localhost/web/api/users/logOut.php').toPromise()
+      .then(() => {
+        this.router.navigate(["/login"]);
+      }
+      )
+  }
 }
 // (:41:)W10D1. cleared, copied to ather files (list.component.ts).
